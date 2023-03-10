39c5696c7f08b8c7f3ef7bf64501dc14ce07e481
anaskaysar <kaysarul.apurba@northsouth.edu>
2023-03-10 11:22:53 +0600

    Done With Basics Recap

--- JS_Recap/README.md
+++ JS_Recap/README.md
@@ -45,7 +45,44 @@ An operator is a special symbol used to perform operations on values and variabl
 - String Operators  - 
 - Other Operator - Ternary operator <b> It returns a value based on condition</b>
 
-###  Type Conversions
+### Type Conversions
 - <b> Implicit Conversion </b> - Known as type coercion where JS itself will automatically convert the type.
 - <b> Explicit Conversion </b> - Where we have to manually convert the type.
 
+### Some Expilicit Conversions
+- Any string to number <b>Number(), parseInt()</b>  
+- Any number to string <b>String(), ().toString() </b>  
+
+### Equality**
+<b> Double Equal (==) </b> - This allows coercion - incase of this js convert the string to same type or number which results in automatic equality.
+<b> Tripple Equal (===) </b> - This does not allow coercion. It is a bit strict in terms of that. It makes sure that the both variables are of smae type before comparing and will not convert automatically.
+
+* It is safe to use <b> === </b>for everyone
+
+### Conditional Statement 
+- if
+- else If
+- else
+- Switch
+
+### Loops
+- For
+- while
+- do while 
+- For of loop -Mostly used for collection of Data like array, no need to keep iteration value. No tension for aray elements access.
+
+### Functions
+- A JavaScript function is a block of code designed  to perform a particular task.
+    <b>Ex: Add two numbers, multiply two numbers etc. </b>
+- Functions are reusable as they can be defined once and can be called with different values  resulting in different results.
+- Functions also devide a complex problem into smaller chunks and makes your program easy to understand and maintain.
+
+* Arrow Functions-
+
+### Scope 
+- Scope basically determines the accessiblity of variables.
+- <b> Block Scope - let and const variables can only be accessed in the blocks they are declared
+- <b> Function Scope
+- <b> Global Scope
+
+* Globaly declared variables can not override the block scope variables.
--- JS_Recap/main.js
+++ JS_Recap/main.js
@@ -48,8 +48,8 @@
 
 // Operator
 
-let x = 10
-let y = "10" //Assignment Operator
+// let x = 10
+// let y = "10" //Assignment Operator
 // console.log(x + y) //Arithmetic Operator
 // console.log(++x) //Arithmetic Operator
 // console.log(--y) //Arithmetic Operator
@@ -83,4 +83,102 @@ let y = "10" //Assignment Operator
 // console.log("Bruce" - "Wane")
 
 
-//
+// Explicit Conversion
+// console.log(Number('5'))
+// console.log(Number(false))
+// console.log(parseInt('5'))
+// console.log(parseFloat('5.11'))
+
+// // String Global Method
+
+// console.log(String(500))
+// console.log(String(undefined))
+// console.log((500).toString())
+// console.log(Boolean(10)) //null undefined 0 '' nan willl always return false others will return true
+
+//Equality
+
+// const var1 = 'test'
+// const var2 = 'test'
+
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+// Conditional Statement
+
+// const num = -5
+// if (num > 0) {
+//     console.log("Number is positive")
+// }
+// else if (num < 0) {
+//     console.log("Number Is negative")
+// }
+// else {
+//     console.log("Number is Zero")
+// }
+
+// const color = 'red'
+// switch (color) {
+//     case 'blue':
+//         console.log("Color is Blue")
+//         break;
+//     case 'red':
+//         console.log("Color is Red")
+//         break;
+//     case 'green':
+//         console.log("Color is Green")
+//         break;
+// }
+
+// looping
+
+const x = 5
+
+//For Loop
+// for (let i = 0; i <= x; i++) {
+//     console.log("Iteration Number " + i)
+// }
+
+//While Loop
+// let i = 0
+// while (i <= x) {
+//     console.log("Iteration Number: " + i)
+//     i++
+// }
+
+//do while - atleast executes one
+
+// let i = 1
+// do {
+//     console.log("The iteration is : " + i)
+//     i++
+// } while (i <= 5)
+
+// For_of loop
+
+// const numArray = [6, 5, 4, 3, 2, 1, 0]
+
+// for (const num of numArray) {
+//     console.log("Iteration Number " + num)
+// }
+
+// Functions
+
+// function greet(x) {
+//     console.log("Good Moring " + x)
+// }
+// greet("Mr X")
+// greet("Mr y")
+// greet("Mr z")
+// greet("Mr k")
+
+
+// const add = (a, b) => {
+//     return a + b
+// }
+// const add = (a, b) => a + b //second way to write arrow function
+// const add5 = a => a + 5 //second way to write arrow function
+
+// console.log(add5(5))
+
+// Scopes
