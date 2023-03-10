0925c1681a4f1e2e6215c56d936e28107d9aa008
anaskaysar <kaysarul.apurba@northsouth.edu>
2023-03-10 09:53:40 +0600

    Update

---
+++ JS_Recap/main.js
@@ -0,0 +1,86 @@
+// //log statement using console.log
+// console.log("Hello From Main.js ")
+
+// //variables
+
+// let age = 25
+// console.log(age)
+
+// const salary = 1000
+// // const salary
+// salary = 5000;
+// console.log(salary)
+
+//Data Types
+// //Seven Primtive Data Types
+// const name = "Kaysar"
+// const language = "JavaScript"
+// const channel = 'Kaysarul Anas'
+
+// const total = 10
+// const PI = 3.1416
+
+// const isPrimeNumber = true
+// const isNewUser = false
+
+// let result
+// // console.log(result) // When a variable is declared but no value is assigned then it is called undefined
+
+// const res = undefined
+// const data = null
+
+
+// //Javascript Object
+
+// const person = {
+//     'firstname': "Bruce",  //key can only be symbol or string where value can be anything
+//     'lastname': "Lee",
+//     'age': 30
+// }
+
+// console.log(person.firstname)
+// console.log(person.lastname)
+
+// // Arraytypes
+
+// const oddNumbers = [1, 3, 5, 7, 9]
+// console.log(oddNumbers[0])  // Array element can be access bia position of the value
+
+// Operator
+
+let x = 10
+let y = "10" //Assignment Operator
+// console.log(x + y) //Arithmetic Operator
+// console.log(++x) //Arithmetic Operator
+// console.log(--y) //Arithmetic Operator
+
+// Comparison Operator
+
+// console.log(x == y)
+// console.log(x != y)
+// console.log(x === y) //it compares not only value but also data type
+// console.log(x !== y) //special equality
+// console.log(x >= y)
+// console.log(x <= y)
+
+// Logical Operator
+// const isValidNumber = x > 20 && 8 < y
+// console.log(isValidNumber)
+
+// Ternary Operator
+
+// const isEvan = 10 % 2 === 0 ? true : false
+// const isEvan2 = 10 % 2 === 0 ? "Even" : "Odd"
+// console.log(isEvan)
+
+// // Type Conversion
+// console.log(true + '3')
+// console.log('6' - '3')
+// console.log('6' * '3')
+// console.log('6' / '3')
+
+// //Not a number
+// console.log("Bruce" - "Wane")
+
+
+//
